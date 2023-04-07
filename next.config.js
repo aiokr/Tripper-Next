0ee6922f943923466a981dc436086a373d79e79c
aiokr <hiaiokr@gmail.com>
2023-04-08 02:25:58 +0800

    add Header

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: ['imgur.lzmun.com'],
+  },
 }
 
 module.exports = nextConfig
---
+++ src/app/about/page.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="container max-w-[800px]">
+      <div className='grid grid-cols-12 gap-6'>
+        <div className='style.blank lg:col-span-2'></div>
+        <div className='style.blank col-span-8 lg:col-span-10 pt-[65px]'>
+          <div className='text-[48px] font-[500] leading-[60px]'>Hey,</div>
+          <div className='text-[48px] font-[500] leading-[60px]'>I am aiokr</div>
+          <div className='py-4'>Always a student / Photographer / Blogger</div>
+        </div>
+      </div>
+      <Image
+        src="https://imgur.lzmun.com/picgo/after2022/202209101335136.png_avatar"
+        alt="Picture of the author"
+        className='center pt-12'
+        width={128}
+        height={128}
+        automatically="true"
+        provided="true"
+      />
+    </main>
+  )
+}
