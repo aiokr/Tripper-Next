aecde18de624aff55ec180d621d1ccaab926fa4d
aiokr <hiaiokr@gmail.com>
2023-04-11 22:28:04 +0800

    添加 cusdis 评论

--- app/post/[slug]/page.js
+++ app/post/[slug]/page.js
@@ -1,18 +1,21 @@
+"use client"
+
 import Head from 'next/head'
 import Link from 'next/link'
 import Script from 'next/script'
 import { format, parseISO } from 'date-fns'
+import React, { useRef } from 'react';
 import { allPosts } from 'contentlayer/generated'
 import style from './post.module.css'
+import { ReactCusdis } from 'react-cusdis'
 var md = require('markdown-it')({
   breaks: true,
 });
 
-
-export default async function PostPage2(props) {
+export default async function PostPage(props) {
   console.log(props.params.slug)
   const post = allPosts.find((post) => post.url == props.params.slug)
-  const url = 'https://next.tripper.press/post/'+props.params.slug
+  const url = 'https://next.tripper.press/post/' + props.params.slug
   const postCont = post.body.raw
   var result = md.render(postCont);
   return (
@@ -20,16 +23,17 @@ export default async function PostPage2(props) {
       <div className='text-3xl font-medium pt-4'>{post.title}</div>
       <div className='opacity-60 pt-4'>{format(parseISO(post.date), 'yyyy-MM-dd')}{post.category ? `/${post.category}` : ''}{post.tags ? `/${post.tags}` : ''}</div>
       <div className='article pt-6' dangerouslySetInnerHTML={{ __html: result }} />
-      <div className={`${style['commentHr']} py-4`}></div>
-      <div id="cusdis_thread"
-        data-host="https://cusdis.com"
-        data-app-id="f72a62c5-d240-4ab2-a094-fdda5a919029"
-        data-page-id={props.params.slug}
-        data-page-url={url}
-        data-page-title={post.title}
-      />
-    <Script src="https://cusdis.com/js/cusdis.es.js" strategy="lazyOnload"/>
-    <div className='py-6'></div>
+      <div className={`${style['commentHr']} pt-10 pb-6`}>
+        <ReactCusdis
+          lang="zh-cn"
+          attrs={{
+            host: 'https://cusdis.com',
+            appId: 'f72a62c5-d240-4ab2-a094-fdda5a919029',
+            pageId: props.params.slug,
+            pageTitle: post.title,
+            pageUrl: url
+          }}
+        /></div>
     </main>
   )
 }
