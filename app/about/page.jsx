0b32128cd0985c5ab2fce97b260c9d43532917db
aiokr <hiaiokr@gmail.com>
2023-05-15 09:35:18 +0800

    fix about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -7,18 +7,18 @@ import Script from 'next/script'
 export default async function AboutPage() {
   return (
     <main className='dark:dark:bg-zinc-900'>
-      <div className='container px-4 lg:px-2 max-w-[1000px] grid grid-cols-12 grid-rows-7 gap-4 pt-[65px]'>
-        <div className={`${style['aboutCard']} relative row-span-2 col-span-6 px-6 py-4 lg:px-10 lg:py-8 bg-main`}>
+      <div className='container px-4 lg:px-2 max-w-[1000px] grid grid-cols-12 grid-rows-8 gap-4 pt-[65px]'>
+        <div className={`${style['aboutCard']} relative row-span-4 col-span-6 px-6 py-4 lg:px-10 lg:py-8 bg-main`}>
           <div className='text-xl lg:text-3xl font-[500] leading-[36px] lg:leading-[48px] text-white'>👋 Hey,</div>
           <div className='text-2xl leading-5 lg:text-[48px] py-1 lg:py-8 font-[500] text-white'>I am aiokr</div>
           <Link href='/about/me' className={`${style['cardBtn']}`}>
             <Image src="../plus.svg" width="20" height="20" />
           </Link>
         </div>
-        <div className={`${style['aboutCard']} row-span-5 col-span-6 p-6 lg:p-12 bg-black`}>
+        <div className={`${style['aboutCard']} row-span-4 col-span-6 p-6 lg:p-12 bg-black`}>
           <div className={`${style['earthCard']} h-full`}></div>
         </div>
-        <div className={`${style['aboutCard']} row-span-3 col-span-6 px-6 py-4 lg:px-10 lg:py-8 border`}>
+        <div className={`${style['aboutCard']} row-span-4 col-span-6 px-6 py-4 lg:px-10 lg:py-8 border`}>
           <div className='text-2xl lg:text-3xl font-[500] leading-[36px] lg:leading-[48px] dark:text-white'>🏳️‍🌈 I am</div>
           <div className='py-1 lg:py-4'>
             <span className='text-lg leading-6 lg:leading-[3rem] lg:text-3xl font-[500] mr-2 bg-clip-text text-transparent bg-gradient-to-r from-[#71afdd70] to-[#71afdd]'>Always as a Student</span><br />
@@ -32,7 +32,7 @@ export default async function AboutPage() {
             </span><br />
           </div>
         </div>
-        <div className={`${style['aboutCard']} relative row-span-2 col-span-12 px-6 py-4 lg:px-10 lg:py-8 text-white bg-gradient-to-br from-[#71DEF5] to-[#7199F5]`}>
+        <div className={`${style['aboutCard']} relative row-span-4 col-span-6 px-6 py-4 lg:px-10 lg:py-8 text-white bg-gradient-to-br from-[#71DEF5] to-[#7199F5]`}>
           <div className='text-2xl lg:text-3xl font-[500] leading-[36px] lg:leading-[48px]'>💡 Intersted in</div>
           <div className='py-1 lg:py-4 flex justify-center'>
             <span className='text-lg leading-6 lg:leading-[3rem] lg:text-3xl font-[500] mr-2'>Digital Nomad</span>
