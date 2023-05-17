6362c9164d46ab7d7b39e5228a9b4d4dc9e3d9e5
aiokr <hiaiokr@gmail.com>
2023-05-18 00:12:34 +0800

    修复移动端跳转到相册页面后，不在顶部的问题 修改移动端样式

--- app/photo/layout.jsx
+++ app/photo/layout.jsx
@@ -10,16 +10,9 @@ export default function PressPage({ children }) {
             width={64} height={64}
             alt="logo" automatically="true" provided="true"
           />
-          <div className={`${style['heroText']} text-lg lg:text-6xl`}>
+          <div className={`${style['heroText']} text-xl lg:text-6xl`}>
             <div className='text-center uppercase'>The Moment When I Press The Tripper</div>
             <div className='text-center uppercase'>The Moment Forever</div>
-          </div>
-
-          <div className='pt-24 block md:hidden'>
-            <span className='text-center'>View Works</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 pt-2 center">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-            </svg>
           </div>
         </div>
       </div>
