968ee68e9b86e16d39e02351eed18207955e2103
aiokr <hiaiokr@gmail.com>
2023-05-18 21:54:31 +0800

    更新照片页面，去掉背景的半透明渐变遮罩（有性能问题） 初步增加 OG API

---
+++ app/api/og/index.jsx
@@ -0,0 +1,31 @@
+import { ImageResponse } from '@vercel/og';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default function OGImage() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 64,
+          background: '#71afdd',
+          color: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Tripper Press
+      </div>
+    ),
+    {
+      width: 600,
+      height: 600,
+    },
+  );
+}
