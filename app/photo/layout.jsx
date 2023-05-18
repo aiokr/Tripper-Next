d84ae5fb8030530e564f9bde0fd7e4f8af895392
aiokr <hiaiokr@gmail.com>
2023-05-18 22:11:01 +0800

    移除视差滚动效果

--- app/photo/layout.jsx
+++ app/photo/layout.jsx
@@ -3,15 +3,17 @@ import Link from 'next/link'
 import style from './photo.module.css'
 export default function PressPage({ children }) {
   return (
-    <main className={`${style['background']} bg-zinc-900 text-white font-thin`}>
-      <div className={`${style['hero']}  px-12 py-16 lg:pt-48 lg:pb-20`} >
-        <Image className="pb-24" src="https://imgur.lzmun.com/picgo/logo/tripper2colorfull.png_avatar"
-          width={64} height={64}
-          alt="logo" automatically="true" provided="true"
-        />
-        <div className={`${style['heroText']} text-xl lg:text-6xl`}>
-          <div className='text-center uppercase'>The Moment When I Press The Tripper</div>
-          <div className='text-center uppercase'>The Moment Forever</div>
+    <main className='bg-zinc-900 text-white font-thin'>
+      <div className={`${style['hero']}`} >
+        <div className={`${style['heroLayer']} px-12 py-16 lg:pt-48 lg:pb-36`}>
+          <Image className="pb-24" src="https://imgur.lzmun.com/picgo/logo/tripper2colorfull.png_avatar"
+            width={64} height={64}
+            alt="logo" automatically="true" provided="true"
+          />
+          <div className={`${style['heroText']} text-xl lg:text-6xl`}>
+            <div className='text-center uppercase'>The Moment When I Press The Tripper</div>
+            <div className='text-center uppercase'>The Moment Forever</div>
+          </div>
         </div>
       </div>
       <div className={`${style['navbar']} container max-w-[600] grid grid-cols-3`}>
