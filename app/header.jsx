d4d617baaebf2274ee018db19c7fd7f152cb3cda
aiokr <hiaiokr@gmail.com>
2023-05-31 00:26:36 +0800

    修改导航栏 修改网页图标

--- app/header.jsx
+++ app/header.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import headerStyle from './header.module.css'
+import style from './header.module.css'
 import darkNavStyle from './headerDark.module.css'
 import Link from 'next/link';
 import Image from 'next/image';
@@ -21,7 +21,7 @@ export default function Header() { // 必须以大写开头
   // 根据当前页面的 pathname 选择要使用的样式
   const currentPageStyle = getPageStyle(pathname);
   return (
-    <div className={`${currentPageStyle.header} ${headerStyle.header} bg-[#ffffff55] dark:text-white dark:bg-[#161617cc]`}>
+    <div className={`${currentPageStyle.header} ${style.header} bg-[#ffffff55] dark:text-white dark:bg-[#161617cc]`}>
       <div className='container grid grid-cols-1 md:grid-cols-3'>
         <Link href="/" className='hidden md:block'>
           <Image src='https://imgur.lzmun.com/picgo/logo/tripper2colorfull.png_avatar' width={32} height={32} unoptimized alt='LOGO'
@@ -29,13 +29,13 @@ export default function Header() { // 必须以大写开头
           />
         </Link>
         <div className='flex justify-center'>
-          <Link className='' scroll={false} href="/">Home</Link>
-          <Link className='' scroll={false} href="/posts/1">Posts</Link>
-          <Link className='' scroll={false} href="/photo">Photography</Link>
-          <Link className='' scroll={false} href="/about">About</Link>
+          <Link className={`${style.headerLink}`} scroll={false} href="/">Home</Link>
+          <Link className={`${style.headerLink}`} scroll={false} href="/posts/1">Posts</Link>
+          <Link className={`${style.headerLink}`} scroll={false} href="/photo">Photography</Link>
+          <Link className={`${style.headerLink}`} scroll={false} href="/about">About</Link>
         </div>
         <div></div>
-        </div>
+      </div>
     </div>
   )
 }
